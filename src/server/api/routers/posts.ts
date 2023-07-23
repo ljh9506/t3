ca06acd0193b03d627632708c8ef9c1474682fd5
ljh9506 <leejh95@nate.com>
2023-07-23 20:32:33 +0900

    feat: modify posts router response & add Profile Component

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,3 +1,5 @@
+import { clerkClient } from "@clerk/nextjs";
+import { type User } from "@clerk/nextjs/dist/types/server";
 import { z } from "zod";
 import {
   createTRPCRouter,
@@ -5,8 +7,30 @@ import {
   protectedProcedure,
 } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profileImageUrl: user.profileImageUrl,
+  };
+};
+
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    return posts.map((post) => ({
+      post,
+      author: users.find((user) => user.id === post.authorId),
+    }));
   }),
 });
